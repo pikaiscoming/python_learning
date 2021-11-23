b7a9f789ae17c232ede60081c5fa1e1643af2532
pikaiscoming <48316662+pikaiscoming@users.noreply.github.com>
2021-11-24 00:47:31 +0800

    Add files via upload

---
+++ Day 35 sent_email(giveup).py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 20:01:29 2021
+
+@author: 皮皮卡卡
+"""
+import ezgmail, os
+#os.chdir(r'D:\Users\pipikaka\Desktop\python\learning_coding')
+ezgmail.init()
