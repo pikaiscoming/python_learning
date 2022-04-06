e2a0539fcea2cd29bcdbc2f27215cd1935c6cecb
pikaiscoming <48316662+pikaiscoming@users.noreply.github.com>
2022-04-06 15:02:25 +0800

    Add files via upload

---
+++ Day_59_subplot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 11:04:09 2022
+
+@author: pikachu
+"""
+
+import matplotlib.pyplot  as plt
+
+fig = plt.figure()
+plt.subplot(221)
+plt.subplot(222)
+plt.subplot(212)
+
+
+
+plt.show()
