743d2f9f9992c0110c838c6000594e1b3f931e2c
pikaiscoming <48316662+pikaiscoming@users.noreply.github.com>
2022-04-21 14:49:57 +0800

    Add files via upload

---
+++ Day_66_plot_coordinates.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 14:32:16 2022
+
+@author: 皮皮卡卡
+"""
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+fig1 = plt.figure()
+ax = fig.gca(projection='3d')
+ax1 = fig1.gca(polar=True)
+plt.show()
+
+#plt.sqvefig('filename.pdf', format='pdf)
